594da7084d47458fcdcefac38e1b36a77c9839a1
Rose Mary <rosemaryjoe007@gmail.com>
2026-04-22 19:27:13 +0530

    updated API URL1

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://posts-website-xvkf.onrender.com",
+  baseURL: "https://posts-website-xvkf.onrender.com/api",
 });
 
 export default API;
